dd681ec98c73e104575abe735de5042ac4e844aa
Ayush Goyal <ayush987goyal@gmail.com>
2019-12-07 10:37:24 +0530

    use basics of suspence with createResource

--- src/exercises/01.js
+++ src/exercises/01.js
@@ -2,55 +2,35 @@
 
 // http://localhost:3000/isolated/exercises/01
 
-import React from 'react'
-import {PokemonDataView} from '../utils'
-// 🐨 you'll need to import the fetchPokemon function
-// 💰 here you go:
-// import fetchPokemon from '../fetch-pokemon'
-// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
+import React, {Suspense} from 'react'
+import fetchPokemon from '../fetch-pokemon'
+import {PokemonDataView, ErrorBoundary, PokemonInfoFallback} from '../utils'
 
-// you'll also need the ErrorBoundary component from utils
-// 💰 here you go:
-// import {ErrorBoundary} from '../utils'
-// 💰 use it like this: <ErrorBoundary><SomeOtherComponents /></ErrorBoundary>
+function createResource(asyncfn) {
+  let result
+  let error
+  let promise = asyncfn()
+    .then(r => (result = r))
+    .catch(e => (error = e))
 
-// By default, all fetches are mocked so we can control the time easily.
-// You can adjust the fetch time with this:
-// window.FETCH_TIME = 3000
-// If you want to make an actual network call for the pokemon
-// then uncomment the following line
-// window.fetch.restoreOriginalFetch()
-// Note that by doing this, the FETCH_TIME will no longer be considered
-// and if you want to slow things down you should use the Network tab
-// in your developer tools to throttle your network to something like "Slow 3G"
-
-// 🐨 create the following mutable variable references (using let):
-// pokemon, pokemonError, pokemonPromise
-
-// 💣 delete this now...
-const pokemon = {
-  name: 'TODO',
-  number: 'TODO',
-  attacks: {
-    special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
-  },
-  fetchedAt: 'TODO',
+  return {
+    read() {
+      if (error) {
+        throw error
+      }
+      if (!result) {
+        throw promise
+      }
+      return result
+    },
+  }
 }
 
-// We don't need the app to be mounted to know that we want to fetch the pokemon
-// named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign the pokemonPromise variable to a call to fetchPokemon('pikachu')
-
-// 🐨 when the promise resolves, set the pokemon variable to the resolved value
-// 🐨 if the promise fails, set the pokemonError variable to the error
+const pokemonResource = createResource(() => fetchPokemon('pikachu'))
 
 function PokemonInfo() {
-  // 🐨 if pokemonError is defined, then throw it here
-  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
-  // 💰 (no, for real. Like: `throw pokemonPromise`)
+  const pokemon = pokemonResource.read()
 
-  // if the code gets it this far, then the pokemon variable is defined and
-  // rendering can continue!
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
@@ -64,12 +44,11 @@ function PokemonInfo() {
 function App() {
   return (
     <div className="pokemon-info">
-      {/*
-        🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback
-        🐨 Then wrap all that with an <ErrorBoundary /> to catch errors
-        💰 I wrote the ErrorBoundary for you. You can take a look at it in the utils file if you want
-      */}
-      <PokemonInfo />
+      <ErrorBoundary>
+        <Suspense fallback={<PokemonInfoFallback name="pikachu" />}>
+          <PokemonInfo />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   )
 }
