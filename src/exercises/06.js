761c39aa5b089041161677f32351c7b73e2143d2
Ayush Goyal <ayush987goyal@gmail.com>
2019-12-08 10:21:40 +0530

    Add custom hook for resource

--- src/exercises/06.js
+++ src/exercises/06.js
@@ -62,25 +62,31 @@ function createPokemonResource(pokemonName) {
   return {data, image}
 }
 
-function App() {
-  const [pokemonName, setPokemonName] = React.useState('')
-  // 🐨 move these two lines to a custom hook called usePokemonResource
-
-  // 🐨 call usePokemonResource with the pokemonName.
-  //    It should return both the pokemonResource and isPending
+function usePokemonResource(pokemonName) {
   const [startTransition, isPending] = React.useTransition(SUSPENSE_CONFIG)
   const [pokemonResource, setPokemonResource] = React.useState(null)
 
+  React.useLayoutEffect(() => {
+    if (!pokemonName) {
+      return
+    }
+
+    startTransition(() => {
+      setPokemonResource(getPokemonResource(pokemonName))
+    })
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pokemonName])
+
+  return [pokemonResource, isPending]
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState('')
+  const [pokemonResource, isPending] = usePokemonResource(pokemonName)
+
   function handleSubmit(newPokemonName) {
     setPokemonName(newPokemonName)
-    // 🐨 move this startTransition call to a useLayoutEffect inside your
-    //    custom usePokemonResource hook (it should list pokemonName as a
-    //    dependency).
-    startTransition(() => {
-      setPokemonResource(getPokemonResource(newPokemonName))
-    })
-    // 💰 tip: in your effect callback, if pokemonName is an empty string,
-    //    return early.
   }
 
   return (
